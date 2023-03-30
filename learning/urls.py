13c2349092266749ccc6b399a84715a633050a90
mateusz <mateuszmikulski2@gmail.com>
2023-03-30 23:48:45 +0200

    Add initial teacher dashboard

--- learning/urls.py
+++ learning/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "learning"
 urlpatterns = [
     path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
+    path('teacher/', views.TeacherDashboard.as_view(), name='teacher_dashboard'),
 ]
--- learning/views.py
+++ learning/views.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Course, Module
 
 
 class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'learning/dashboard.html'
 
+
+class TeacherDashboard(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'learning/teacher/teacher_dashboard.html'
+    
+    def get_queryset(self):
+        return Course.objects.filter(owner=self.request.user)
