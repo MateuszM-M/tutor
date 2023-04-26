d1e83b8266ba3217b7e561b43a7b691fbd9977d5
mateusz <mateuszmikulski2@gmail.com>
2023-04-26 20:50:03 +0200

    Make learning views more DRY

--- learning/views.py
+++ learning/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.utils.text import slugify
 from django.views.generic import CreateView, TemplateView, UpdateView
 from django.views.generic.detail import DetailView
@@ -11,6 +12,40 @@ from .forms import CreateUpdateCourseForm
 from .models import Course
 
 
+class OwnerMixin(object):
+    """
+    A mixin that overrides the queryset to filter objects by request user
+    """
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+    
+
+class OwnerEditMixin(object):
+    """
+    A mixin for creation and update, 
+    which assings course owner to request user
+    """
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
+    """
+    A mixin that assing model to course
+    """
+    model = Course
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    """
+    A mixin that assings form and succes_url
+    """
+    form_class = CreateUpdateCourseForm
+    success_url = reverse_lazy('learning:teacher_dashboard')
+
+
 class Dashboard(LoginRequiredMixin, TemplateView):
     """
     A class to represent main dashboard view.
@@ -18,67 +53,52 @@ class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'learning/dashboard.html'
 
 
-class TeacherDashboard(LoginRequiredMixin, FilterView):
+class TeacherDashboard(OwnerCourseMixin, FilterView):
     """
     A class to represent teacher dashboard view.
     """
-    model = Course
     template_name = 'learning/teacher/teacher_dashboard.html'
     paginate_by = 10
     filterset_class = CourseFilter
-    
-    def get_queryset(self):
-        """
-        Returns a list of Courses creasted by request user.
-        """
-        return Course.objects.filter(owner=self.request.user)
+        
 
-
-class CreateCourseView(LoginRequiredMixin, CreateView):
+class CreateCourseView(OwnerCourseEditMixin, CreateView):
     """
     A class to represent create course view.
     """
-    model = Course
-    form_class = CreateUpdateCourseForm
     template_name = 'learning/teacher/create_course.html'
     success_url = '/teacher/'
 
     def form_valid(self, form):
         """
-        Ovverides the form validation to addtionally assign
-        owner of the course to request user and
+        Overides the form validation to addtionally assign
         slug from slugyfying the course title
         """
-        form.instance.owner = self.request.user
         form.instance.slug = slugify(form.instance.title)
-        return super().form_valid(form)
+        return super(CreateCourseView, self).form_valid(form)
     
 
-class CourseDeleteView(DeleteView, LoginRequiredMixin):
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
     """
     A class to represent delete couse view.
     """
-    model = Course
     success_url = '/teacher/'
     template_name = 'learning/teacher/delete_course.html'
 
 
-class CourseUpdateView(UpdateView, LoginRequiredMixin):
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
     """
     A class to represent update couse view.
     """
-    model = Course
-    form_class = CreateUpdateCourseForm
     template_name = 'learning/teacher/update_course.html'
     success_url = '/teacher/'
 
 
-class CourseDetailView(DetailView, LoginRequiredMixin):
+class CourseDetailView(OwnerCourseMixin, DetailView):
     """
     A class to represent detail course view for user that
     is interested in buying a course.
     """
-    model = Course
     template_name = 'learning/student/course_overview.html'
 
     def get_context_data(self, **kwargs):
